46bbb418d8b9d7f822e3f6649e88b35b666e345b
Patrick R North <60725747+patrickrnorth@users.noreply.github.com>
2021-10-29 22:39:18 -0400

    Consume the API from the Client

---
+++ Client/Static/APIEndpoints.cs
@@ -0,0 +1,14 @@
+﻿namespace Client.Static
+{
+    public class APIEndpoints
+    {
+#if DEBUG
+        internal const string ServerBaseUrl = "https://localhost:5003";
+
+#else
+        interal const string ServerBaseUrl = "https://appname.azurewebsites.net";
+#endif
+
+        internal readonly static string s_categories = $"{ServerBaseUrl}/api/categories";
+    }
+}
